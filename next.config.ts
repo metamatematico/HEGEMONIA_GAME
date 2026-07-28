9ced2db499382e73ff05a099fdf0c311c0dc833e
Z User <z@container>
2026-07-28 05:42:42 +0000

    2d3bcb31-df32-4bc4-8ffe-370960082bcb

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactStrictMode: false,
-  turbopack: {
-    root: "/home/z/my-project",
-  },
 };
 
 export default nextConfig;
